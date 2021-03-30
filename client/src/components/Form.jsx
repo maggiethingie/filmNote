7d1eaab5ca8be2f7c1d6c9cbd8b149a7abde59ab
Maggie Aytac <maggie.aytac@gmail.com>
2021-03-30 12:08:05 -0700

    ideas icon working

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Ideas from './Ideas.jsx';
 
 class Form extends React.Component {
   constructor(props) {
@@ -6,6 +7,16 @@ class Form extends React.Component {
     this.state = {
       showIdeas: false
     }
+    this.escFunction = this.escFunction.bind(this);
+    this.hideIdeas = this.hideIdeas.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.escFunction, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.escFunction, false);
   }
 
   onFormSubmit() {
@@ -17,13 +28,27 @@ class Form extends React.Component {
     this.setState({ showIdeas: true });
   }
 
+  escFunction(event) {
+    if (event.keyCode === 27) {
+      this.hideIdeas();
+    }
+  }
+
+  hideIdeas() {
+    if (this.state.showIdeas) {
+      this.setState({ showIdeas: false });
+    }
+  }
+
   render() {
     const { filmObj } = this.props;
+    const { showIdeas } = this.state;
     return (
       <div className="form">
         <h2>{filmObj.Title} ({filmObj.Year})</h2>
         <form>
-          <img onClick={this.onIdeaClick} className="idea-icon" src="icons/idea.svg" />
+          { showIdeas ? <Ideas hideIdeas={this.hideIdeas} /> : null }
+          <img onClick={() => this.onIdeaClick()} className="idea-icon" src="icons/idea.svg" />
           <textarea className="textarea" rows="50" cols="40" />
           <button onClick={this.onFormSubmit}>Save and Exit</button>
         </form>
